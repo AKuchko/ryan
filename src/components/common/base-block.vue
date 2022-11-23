c0e8ec311db2024f2fe531c743b7f50a876002dc
AK <101558236+AKuchko@users.noreply.github.com>
2022-11-23 20:55:42 +0700

    base-block fixed

--- src/components/common/base-block.vue
+++ src/components/common/base-block.vue
@@ -12,16 +12,21 @@ export default {
 
 <style lang="scss">
 .base-block {
-    width: 100%;
-    padding: 30px 20px;
+    padding: 20px;
     background: $color-secondary-dark;
     border-radius: 10px;
     box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.2);
-}
+    transition: all 0.1s ease;
 
+    @media (min-width: 768px) {
+        padding: 30px;
+    }
 
-@media (prefers-color-scheme: light) {
-    .base-block {
+    @media (min-width: 1440px) {
+        padding: 40px;
+    }
+
+    @media (prefers-color-scheme: light) {
         background: $color-secondary-light;
         box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
     }
